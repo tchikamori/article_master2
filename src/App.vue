2c6b6e43718941931a5493bc2bddb02c3cde0163
tchikamori <takao.chikamori@gmail.com>
2025-10-15 14:31:10 +0900

    br1 fisrt change

--- src/App.vue
+++ src/App.vue
@@ -1,7 +1,7 @@
 <script setup></script>
 
 <template>
-  <h1>First Modification2</h1>
+  <h1>First Modification2 ** a branch1 change **</h1>
   <p>
     Visit <a href="https://vuejs.org/" target="_blank" rel="noopener">vuejs.org</a> to read the
     documentation
