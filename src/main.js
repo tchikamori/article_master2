0bde0a0fc68f716d193c370c7377eaddc67cc8a2
tchikamori <takao.chikamori@gmail.com>
2025-10-15 17:24:40 +0900

    PrimeVue added

--- src/main.js
+++ src/main.js
@@ -1,4 +1,17 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+import PrimeVue from 'primevue/config'
+import ContextMenu from 'primevue/contextmenu';
+import Textarea from 'primevue/textarea';
+import Button from 'primevue/button';
+import Select from 'primevue/select';
+import Menubar from 'primevue/menubar';
 
-createApp(App).mount('#app')
+const app = createApp(App)
+app.use(PrimeVue)
+app.component('ContextMenu', ContextMenu);
+app.component('Textarea', Textarea);
+app.component('Button', Button);
+app.component('Select', Select);
+app.component('Menubar', Menubar);
+app.mount('#app')
